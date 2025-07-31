fe548a229212cafe214810f9d0eb00580928c740
TTubi <Agustinsingh89@hotmail.com>
2025-07-31 17:52:31 -0300

    Crear admin

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.create_superuser('admin', 'admin@email.com', 'admin123')" \
+| python manage.py shell
