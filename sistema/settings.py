95e16c0006a9d4310a149444133d6cf01bf4a4b7
TTubi <Agustinsingh89@hotmail.com>
2025-07-31 17:39:16 -0300

    Usar PostgreSQL y migrar en Render

--- sistema/settings.py
+++ sistema/settings.py
@@ -6,10 +6,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(dotenv_path=os.path.join(BASE_DIR, '.env'))
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -65,15 +62,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sistema.wsgi.application'
 
 
+load_dotenv(dotenv_path=os.path.join(BASE_DIR, '.env'))
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
