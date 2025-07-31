ebad5b21f336cb609fc6b8a8fbcae9cf298e2086
TTubi <Agustinsingh89@hotmail.com>
2025-07-31 18:23:43 -0300

    crear empleado

--- build.sh
+++ build.sh
@@ -5,3 +5,17 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "
+from django.contrib.auth import get_user_model
+from tareas_app.models import Empleado
+User = get_user_model()
+user = User.objects.get(username='admin')
+if not Empleado.objects.filter(usuario=user).exists():
+    Empleado.objects.create(
+        usuario=user,
+        nombre='Administrador',
+        apellido='Principal',
+        perfil='administrador'
+    )
+" | python manage.py shell
